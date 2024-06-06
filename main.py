ce5b2e6158f0d49d10506e07871d6414310f3206
otherworldlyguardian <otherworldlyguardian@gmail.com>
2024-06-06 14:30:26 -0400

    B: added letter count

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ def main():
     path_to_book = "books/frankenstein.txt"
     book_text = get_contents(path_to_book)
     print(f"{count_words(book_text)} words in this book")
+    print(count_letters(book_text))
 
 def get_contents(path):
     with open(path) as f:
@@ -10,5 +11,15 @@ def get_contents(path):
 def count_words(sample_text):
     word_list = sample_text.split()
     return len(word_list)
+
+def count_letters(sample_text):
+    all_lower = sample_text.lower()
+    letters = {}
+    for ltr in all_lower:
+        if ltr not in letters:
+            letters[ltr] = 1
+        else:
+            letters[ltr] += 1
+    return letters
 
 main()
